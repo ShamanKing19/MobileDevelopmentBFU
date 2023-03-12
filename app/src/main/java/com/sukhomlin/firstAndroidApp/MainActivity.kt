4073cbeeb04ad64316083b9caf2dea2bbbd4aeea
g.sukhomlin@skillline.ru <g.sukhomlin@skillline.ru>
2023-03-12 17:58:14 +0200

    initial commit

---
+++ app/src/main/java/com/sukhomlin/firstAndroidApp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sukhomlin.firstAndroidApp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
